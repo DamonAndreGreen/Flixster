c04e9acb4c79f215d59c90640a2f91c0d841e311
Damon <Pdamongreen1399@gmail.com>
2020-02-18 02:37:19 -0500

    2/18/2020

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Damon Andre Green on 2/18/20.
+//  Copyright © 2020 Damon Andre Green. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
